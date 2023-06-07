28a598c090a9a0401cc8d997183d9a67eb8c0e81
aaronallencs <imaaronacs@gmail.com>
2023-06-07 15:41:20 -0700

    initial

---
+++ src/main/java/com/aot/AOTUser/AotUserApplication.java
@@ -0,0 +1,13 @@
+package com.aot.AOTUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AotUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AotUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aot/AOTUser/AotUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aot.AOTUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AotUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
